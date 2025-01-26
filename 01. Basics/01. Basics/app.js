3706cfcb58c88c73697e138d8838a115018fd968
KvassAndVodka <javier.raut@gmail.com>
2025-01-26 23:42:21 +0800

    Added basics

---
+++ 01. Basics/01. Basics/app.js
@@ -0,0 +1,15 @@
+// 1. Create a variable name (name) & store your name.
+// 2. Create a variable name (dreamJob) & store your dream job.
+// 3. Create a variable name (gender) & store your gender.
+// 4. Create a variable name (github) & store your github username.
+// 5. Finally log all the variables in the console.
+
+let name = "Javier Raut";
+let dreamJob = "Software Engineer";
+let gender = "Male";
+let github = "KvassAndVodka";
+
+console.log(name);
+console.log(dreamJob);
+console.log(gender);
+console.log(github);
