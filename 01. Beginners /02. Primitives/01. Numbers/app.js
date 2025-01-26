65b06f333cc9b8aeee15bbed9ccfa98f970d67b3
KvassAndVodka <javier.raut@gmail.com>
2025-01-27 00:46:07 +0800

    Restructured folders and added /primitives/numbers

---
+++ 01. Beginners /02. Primitives/01. Numbers/app.js
@@ -0,0 +1,29 @@
+// 1. Create Variable name (firstFavNum) & store your favorite number.
+// 2. Create Variable name (secondFavNum) & store your second fav number.
+// 3. Add (firstFavNum & secondFavNumber).
+// 4. Subtract (firstFavNum & secondFavNumber).
+// 5. Multiply (firstFavNum & secondFavNumber).
+// 6. Divided (firstFavNum & secondFavNumber).
+// 7. Check (firstFavNum Mod secondFavNumber)
+// 8. Check the power of (firstFavNum)
+
+let firstFavNum = 9;
+let secondFavNum = 10;
+
+// Addition
+console.log(firstFavNum + secondFavNum);
+
+// Subtraction
+console.log(firstFavNum - secondFavNum);
+
+// Multiplication
+console.log(firstFavNum * secondFavNum);
+
+// Division
+console.log(firstFavNum / secondFavNum);
+
+// Modulo
+console.log(firstFavNum % secondFavNum);
+
+// Power
+console.log(firstFavNum ** secondFavNum);
